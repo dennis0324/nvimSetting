cf64a056553e0612eaf90de53e0a08640e671ec6
dennis ko <dennis0324@naver.com>
2023-10-21 00:57:09 +0900

    add: key binding for coding

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,52 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   opts = function(_, opts)
+    opts.highlight = {
+      enable = true,
+    }
+    opts.indent = { enable = true }
+
+    opts.autotag = { enable = true }
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "lua"
+      "json",
+      "javascript",
+      "typescript",
+      "tsx",
+      "yaml",
+      "html",
+      "css",
+      "prisma",
+      "markdown",
+      "markdown_inline",
+      "svelte",
+      "graphql",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
+      "query",
     })
+
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    }
+
+    opts.context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    }
   end,
 }
