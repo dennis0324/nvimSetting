27b326b834eb2d0ad4bccf40864c68357578b1ff
dennisko <dennis0324@naver.com>
2024-02-04 22:23:36 +0900

    change: javascript formatter js to clang-format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local autocmd = vim.api.nvim_create_autocmd
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "JavaScript" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+autocmd({ "BufWritePost" }, {
+  pattern = { "*.js" },
+  callback = function(ev)
+    -- local handle = io.popen("clang-format -i " .. vim.fn.expand("%"))
+    -- if handle ~= nil then
+    --   handle:close()
+    -- end
+    os.execute("clang-format -i " .. vim.fn.expand("%"))
+    vim.cmd("e!")
+    vim.cmd("redraw!")
+  end,
+})
